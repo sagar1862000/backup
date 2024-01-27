37f047e2f6141ba8c84e1cb37260cabaab5fe006
Tuhin Bhowmick <tuhin.b@botshreyasi.com>
2024-01-27 13:50:25 +0530

    call details report changes added

--- projects/candidate-call-report/src/app/call-detail-report/call-detail-report.module.ts
+++ projects/candidate-call-report/src/app/call-detail-report/call-detail-report.module.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { FormsModule } from '@angular/forms';
+import { GridModule } from '../../../../../projects/grid/src/app/grid/grid.module'
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import { FormsModule } from '@angular/forms';
     MatSelectModule,
     MatDatepickerModule,
     FormsModule,
+    GridModule
     
   ]
 })
