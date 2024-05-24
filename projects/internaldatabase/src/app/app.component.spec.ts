b9fde95cb02ddef4ca111c2981e204814a331e5b
BOT Shreyasi <Sagar Sharma>
2024-05-24 11:11:35 +0530

    first commit

--- projects/internaldatabase/src/app/app.component.spec.ts
+++ projects/internaldatabase/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'history'`, () => {
+  it(`should have as title 'internaldatabase'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('history');
+    expect(app.title).toEqual('internaldatabase');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('history app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('internaldatabase app is running!');
   });
 });
