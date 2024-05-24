07af345e37e045c6e26af5095550efccc9dacfb1
BOT Shreyasi <Sagar Sharma>
2024-05-24 11:09:58 +0530

    first commit

--- projects/history/src/app/app.component.spec.ts
+++ projects/history/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hiring-manager'`, () => {
+  it(`should have as title 'history'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hiring-manager');
+    expect(app.title).toEqual('history');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hiring-manager app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('history app is running!');
   });
 });
