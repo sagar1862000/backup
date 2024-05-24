f0da3005bd42cc531a887d0bc0291681ddba47e7
BOT Shreyasi <Sagar Sharma>
2024-05-24 11:01:56 +0530

    first commit

--- projects/control/src/app/app.component.spec.ts
+++ projects/control/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'component'`, () => {
+  it(`should have as title 'control'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('component');
+    expect(app.title).toEqual('control');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('component app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('control app is running!');
   });
 });
